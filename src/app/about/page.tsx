ac9f96e4df607d4909e7dde868d2169a68300ae2
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-29 01:11:20 +0600

    [ADD] photo text added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,6 +8,10 @@ function About() {
 				<div className="flex flex-col gap-10">
 					<div className=" w-full h-[300px] relative">
 						<Image src="/about.jpeg" className='object-cover w-full rounded' fill alt='desktop' />
+						<div className=" absolute left-4 bottom-4 flex flex-col gap-[10px] text-white rounded-md bg-green-500 p-4">
+							<h2 className="text-2xl font-bold">Digital Storytellers</h2>
+							<h3 className="text-xl font-medium">Handcrafting award winning digital experiences</h3>
+						</div>
 					</div>
 				</div>
 			</div>
