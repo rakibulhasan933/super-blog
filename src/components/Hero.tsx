552ce18454bb83718a4ada682c4b59ecc1bd938c
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-28 18:09:57 +0600

    [ADD] hero added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,8 @@ function Hero() {
 			<div className="min-h-screen hero bg-base-200">
 				<div className="flex-col hero-content lg:flex-row-reverse">
 					<Image src="/hero.png" height={250} width={280} priority className="object-contain w-full rounded-lg shadow-2xl" alt='hero' />
-					<div>
-						<h1 className="px-4 text-5xl font-bold">Better Design For your digital Products!</h1>
+					<div className='px-4'>
+						<h1 className="text-5xl font-bold">Better Design For your digital Products!</h1>
 						<p className="py-6">Turning your idea Reality Web bring together the teams the global tech industry </p>
 						<button className="btn btn-primary">See Our Works</button>
 					</div>
