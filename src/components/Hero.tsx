633282ede43a91927d3a65bb278978a8242579cc
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-28 18:08:33 +0600

    [ADD] hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Hero() {
+	return (
+		<div>
+			<div className="min-h-screen hero bg-base-200">
+				<div className="flex-col hero-content lg:flex-row-reverse">
+					<Image src="/hero.png" height={250} width={280} priority className="object-contain w-full rounded-lg shadow-2xl" alt='hero' />
+					<div>
+						<h1 className="px-4 text-5xl font-bold">Better Design For your digital Products!</h1>
+						<p className="py-6">Turning your idea Reality Web bring together the teams the global tech industry </p>
+						<button className="btn btn-primary">See Our Works</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
