c583ac372ab5b438d671624910f81886ae072cdf
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-29 01:01:20 +0600

    [ADD] about page photo added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 import React from 'react'
 
 function About() {
 	return (
-		<div>About</div>
+		<div>
+			<div className="">
+				<div className="flex flex-col gap-10">
+					<div className=" w-full h-[300px] relative">
+						<Image src="/about.jpeg" className='object-cover w-full rounded' fill alt='desktop' />
+					</div>
+				</div>
+			</div>
+		</div>
 	)
 }
 
