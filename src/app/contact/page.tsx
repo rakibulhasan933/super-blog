0de7556a1272c02a40fe1eab1e0abd1a0c4aed59
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-30 23:52:50 +0600

    [ADD] contact page added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,13 +4,20 @@ import React from 'react'
 function Contact() {
 	return (
 		<div className='py-10'>
-			<h1 className="py-4 text-6xl font-bold text-center"> Let,s keep in Touch</h1>
-			<div className="flex flex-row ">
-				<div className="basis-1/2">
+			<div className="my-10 ">
+				<h1 className="py-4 text-5xl font-bold text-center"> Let,s keep in Touch</h1>
+			</div>
+			<div className="flex flex-row gap-5">
+				<div className="p-12 basis-1/2">
 					<Image src="/contact.png" alt='contact' width={400} height={200} className='object-contain ' />
 				</div>
 				<div className="basis-1/2">
-					login Form
+					<div className="flex flex-col gap-5 ">
+						<input type="email" name="email" className='w-4/5 px-4 py-2 border-2 border-teal-400 rounded-md ' placeholder='example@gmail.com' />
+						<input type="text" placeholder='Name' className='w-4/5 px-4 py-2 border-2 border-teal-400 rounded-md ' />
+						<textarea typeof='string' cols={50} rows={10} className='w-4/5 px-4 py-2 border-2 border-teal-400 rounded-md ' placeholder='Enter your message'></textarea>
+						<button className="flex justify-start px-6 py-2 font-bold border-2 rounded-md bg-gradient-to-r from-green-400 to-blue-500 max-w-fit hover:bg-green-500 hover:text-white">Submit</button>
+					</div>
 				</div>
 			</div>
 		</div>
