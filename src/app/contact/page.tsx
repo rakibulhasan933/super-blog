1129e33c46989ac8495f83b739e0cb05eb0af032
rakibulhasan933 <rakibulmd933@gmail.com>
2023-06-30 22:50:29 +0600

    [ADD] contact page update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Contact() {
 	return (
-		<div>Contact</div>
+		<div className='py-10'>
+			<h1 className="py-4 text-6xl font-bold text-center"> Let,s keep in Touch</h1>
+			<div className="flex flex-row ">
+				<div className="basis-1/2">
+					<Image src="/contact.png" alt='contact' width={400} height={200} className='object-contain ' />
+				</div>
+				<div className="basis-1/2">
+					login Form
+				</div>
+			</div>
+		</div>
 	)
 }
 
