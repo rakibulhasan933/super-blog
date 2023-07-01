5d081019d8ab073c981b000529b9f5712a0910ce
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-02 00:25:36 +0600

    [ADD] portfolio

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Portfolio() {
 	return (
-		<div>Portfolio</div>
+		<div className='my-20'>
+			<div className="px-8 my-4">
+				<h1 className="text-5xl font-bold">Our Works</h1>
+				<p className="px-2 text-base font-bold ">Choose a Gallery</p>
+			</div>
+		</div>
 	)
 }
 
