d706b68a9dbb88405ce34640f21e2dcdca2723cd
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-03 16:01:53 +0600

    [ADD] form added

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -15,10 +15,10 @@ function DarkModeToggle() {
     return null
   }
   return (
-    <select value={theme} onChange={e => setTheme(e.target.value)}>
-      <option value="system">System</option>
-      <option value="dark">Dark</option>
-      <option value="light">Light</option>
+    <select value={theme} className='px-2 font-semibold bg-transparent rounded-md ' onChange={e => setTheme(e.target.value)}>
+      <option className='font-medium rounded-md' value="system">System</option>
+      <option className='font-medium rounded-md' value="dark">Dark</option>
+      <option className='font-medium rounded-md' value="light">Light</option>
     </select>
   )
 }
