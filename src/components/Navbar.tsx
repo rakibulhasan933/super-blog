26933eec0306c1647a59390ba4f4745923b8b5cf
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-03 16:22:13 +0600

    [ADD] button hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,12 +14,15 @@ function Navbar() {
 							<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 						</label>
 						<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/">Home</Link></li>
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/portfolio">Portfolio</Link></li>
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/blog">Blog</Link></li>
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/about">About</Link></li>
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/contact">Contact</Link></li>
-							<li className='hover:font-semibold hover:text-green-500'><Link href="/dashboard">DashBoard</Link></li>
+							<li>
+								<DarkModeToggle />
+							</li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/">Home</Link></li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/portfolio">Portfolio</Link></li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/blog">Blog</Link></li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/about">About</Link></li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/contact">Contact</Link></li>
+							<li className='hover:font-bold hover:text-blue-500'><Link href="/dashboard">DashBoard</Link></li>
 							<li className='font-bold text-white rounded bg-gradient-to-r from-green-400 to-blue-500 group'><Link href="/login">
 								Login
 							</Link></li>
@@ -29,15 +32,15 @@ function Navbar() {
 				</div>
 				<div className="hidden navbar-center lg:flex">
 					<ul className="items-center px-1 menu menu-horizontal">
-						<li className='px-2'>
+						<li>
 							<DarkModeToggle />
 						</li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/">Home</Link></li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/portfolio">Portfolio</Link></li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/blog">Blog</Link></li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/about">About</Link></li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/contact">Contact</Link></li>
-						<li className='hover:font-semibold hover:text-green-500'><Link href="/dashboard">DashBoard</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/">Home</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/portfolio">Portfolio</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/blog">Blog</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/about">About</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/contact">Contact</Link></li>
+						<li className='hover:font-bold hover:text-blue-500'><Link href="/dashboard">DashBoard</Link></li>
 						<li className='font-bold text-white rounded bg-gradient-to-r from-green-400 to-blue-500 group'><Link href="/login">
 							Login
 						</Link></li>
