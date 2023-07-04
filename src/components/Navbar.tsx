9b2c08b775522dad7c9a10ab4691229a8d0f38fd
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-04 22:40:54 +0600

    [ADD] navbar item removed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,10 @@
 import Link from 'next/link'
 import React from 'react'
 import DarkModeToggle from './DarkModeToggle';
-import { signIn, useSession } from 'next-auth/react';
 
 
 function Navbar() {
-	const { data: session, status } = useSession()
-	console.log(session)
+
 	return (
 		<div>
 			<div className="navbar bg-base-100">
@@ -44,7 +42,7 @@ function Navbar() {
 						<li className='hover:font-bold hover:text-blue-500'><Link href="/about">About</Link></li>
 						<li className='hover:font-bold hover:text-blue-500'><Link href="/contact">Contact</Link></li>
 						<li className='hover:font-bold hover:text-blue-500'><Link href="/dashboard">DashBoard</Link></li>
-						<li className='font-bold text-white rounded bg-gradient-to-r from-green-400 to-blue-500 group'><button onClick={() => signIn("google")}>
+						<li className='font-bold text-white rounded bg-gradient-to-r from-green-400 to-blue-500 group'><button>
 							Login
 						</button></li>
 					</ul>
