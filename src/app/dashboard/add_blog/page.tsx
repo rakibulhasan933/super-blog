4b1e3d13ce685b988b7b4d628c3e46afd8436b62
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-09 21:06:08 +0600

    [ADD] input from in create blog page

--- src/app/dashboard/add_blog/page.tsx
+++ src/app/dashboard/add_blog/page.tsx
@@ -1,9 +1,31 @@
+"use client"
 import React from 'react'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+	title: string,
+	imageUrl: string,
+	description: string,
+};
 
 function AddBlog() {
+	const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
 	return (
-		<div>
-			<h2>Blog</h2>
+		<div className='flex mx-auto'>
+			<h2 className='text-3xl font-bold'>Add Blogs</h2>
+			<div className="">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input {...register("title", { required: true })} />
+					{errors.title && <span>This field is required</span>}
+					<input {...register("imageUrl", { required: true })} />
+					{errors.imageUrl && <span>This field is required</span>}
+					<input {...register("description", { required: true })} />
+					{errors.description && <span>This field is required</span>}
+					<input type="submit" />
+				</form>
+			</div>
 		</div>
 	)
 }
