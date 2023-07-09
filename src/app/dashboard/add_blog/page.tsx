ba490e3492d5f4860f1f8706a4f3a0c87ddc8b94
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-10 00:27:37 +0600

    [ADD]create blog added

--- src/app/dashboard/add_blog/page.tsx
+++ src/app/dashboard/add_blog/page.tsx
@@ -14,17 +14,20 @@ function AddBlog() {
 
 	return (
 		<div className='mx-[40px]'>
-			<h2 className='mb-10 text-xl font-semibold text-center'>Add Blogs</h2>
 			<div className='flex flex-col px-6'>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<input className='w-full px-2 py-1 my-2 border-2 border-blue-500 rounded-md bg-slate-200' {...register("title", { required: true })} />
-					{errors.title && <span className='my-2 text-red-600'>This field is required</span>}
-					<input className='w-full my-2 border-2 border-blue-500 rounded-md bg-slate-200' {...register("imageUrl", { required: true })} />
-					{errors.imageUrl && <span className='text-red-600'>This field is required</span>}
-					<textarea className='w-full my-2 border-2 border-blue-500 rounded-md bg-slate-200' {...register("description", { required: true })} />
-					{errors.description && <span className='text-red-600'>This field is required</span>}
-					<input type="submit" />
-				</form>
+				<h2 className='mb-10 text-xl font-semibold'>Add Blogs</h2>
+				<div className="flex flex-col gap-2">
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<input className='w-full px-4 py-2 my-2 border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Title' {...register("title", { required: true })} />
+						{errors.title && <span className='my-2 text-red-600'>This field is required</span>}
+						<input className='w-full px-4 py-2 my-2 border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your Image url' {...register("imageUrl", { required: true })} />
+						{errors.imageUrl && <span className='text-red-600'>This field is required</span>}
+						<textarea className='w-full px-4 py-2 my-2 border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your message' {...register("description", { required: true })} />
+						<br />
+						{errors.description && <span className='text-red-600'>This field is required</span>}
+						<input className='px-4 py-2 my-3 font-bold border-2 border-blue-500 rounded-md max-w-fit hover:bg-blue-600 hover:text-white' type="submit" />
+					</form>
+				</div>
 			</div>
 		</div>
 	)
