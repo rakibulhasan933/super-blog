14ddeddcbf7e4287f2a1420f03bccf2189ea8120
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-09 13:03:00 +0600

    [ADD] user get all

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -8,7 +8,7 @@ export const GET = async (request: any) => {
 
 	try {
 		await connect();
-		const users: any = await User.find()
+		const users: any = await User.find();
 		return new NextResponse(users, { status: 200 });
 	} catch (error) {
 		return new NextResponse("Database Error", { status: 500 })
