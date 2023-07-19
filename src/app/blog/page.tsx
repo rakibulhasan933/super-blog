5eff1fdd65b19cb17344166294bbf24015adf04a
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-19 23:28:50 +0600

    [ADD] blog cart added

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
 function Blog() {
 	return (
-		<div>
-			<h2>Welcome to Blog Page</h2>
-			<p>Product page Updated</p>
-			<h4>updated store house 420</h4>
-			<h5>Pull request</h5>
+		<div className='m-10'>
+			<div className="grid grid-cols-3 gap-4">
+				<div className="flex flex-col px-2 border-2 rounded-md ">
+					<div className="flex justify-center">
+						<Image src="/hero.png" alt='photo' width={200} priority height={150} />
+					</div>
+					<p className="my-2 text-base font-normal">Rakibul Hasan</p>
+					<h2 className="my-3 text-xl font-semibold">How to Get a Github Student Developer Pack</h2>
+					<p className="mb-2 text-xs font-normal">May 24,2023</p>
+				</div>
+			</div>
 		</div>
 	)
 }
