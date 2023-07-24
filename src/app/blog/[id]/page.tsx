0ac59a755bd48787eee4f71f2164db0a1f24c46a
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-25 00:18:03 +0600

    [ADD] loading spinner added

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,10 +1,31 @@
-import { ParamsIProps } from '@/type'
+import { ParamsIProps } from '@/type';
 import React from 'react'
 
-function Blog({ params }: ParamsIProps) {
+
+async function getBlog(id: string) {
+	const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+		cache: "no-store"
+	});
+	if (!res.ok) {
+		throw new Error("Failed to Fetch Data")
+	}
+	return res.json();
+};
+
+async function Blog({ params }: ParamsIProps) {
 	const { id } = params;
+	const blog = await getBlog(id);
+	console.log(blog);
 	return (
-		<div>Welcome to Single Blog Page :{id} </div>
+		<div>
+			<div className="flex items-center justify-center">
+				<h2 className="my-52">
+					<div>
+						<h1 className="w-20 h-20 bg-pink-800 loading loading-ring"></h1>
+					</div>
+				</h2>
+			</div>
+		</div>
 	)
 }
 
