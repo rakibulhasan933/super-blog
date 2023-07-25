453cf0ad129fbf8f6de5d539c1b2e286b2159e4f
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-25 19:32:29 +0600

    [ADD] navbar active added

--- src/app/dashboard/add_blog/page.tsx
+++ src/app/dashboard/add_blog/page.tsx
@@ -33,14 +33,14 @@ function AddBlog() {
 	return (
 		<div className='mx-[40px] h-screen '>
 			<div className='flex flex-col px-6'>
-				<h2 className='mb-10 text-xl font-semibold'>Add Blogs</h2>
+				<h2 className='mb-10 text-2xl font-semibold'>Add Blogs</h2>
 				<div className="flex flex-col gap-2">
 					<form onSubmit={handleSubmit(onSubmit)}>
-						<input className='w-full px-4 py-2 my-2 font-mono text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Title' {...register("title", { required: true })} />
+						<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Title' {...register("title", { required: true })} />
 						{errors.title && <span className='my-2 text-red-600'>This field is required</span>}
-						<input className='w-full px-4 py-2 my-2 font-mono text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your Image url' {...register("imageUrl", { required: true })} />
+						<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your Image url' {...register("imageUrl", { required: true })} />
 						{errors.imageUrl && <span className='text-red-600'>This field is required</span>}
-						<textarea typeof='string' cols={20} rows={5} className='w-full px-4 py-2 my-2 font-mono text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your message' {...register("description", { required: true })} />
+						<textarea typeof='string' cols={20} rows={5} className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your message' {...register("description", { required: true })} />
 						<br />
 						{errors.description && <span className='text-red-600'>This field is required</span>}
 						<br />
