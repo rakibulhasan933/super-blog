74f8bbd9375ab7366caef743e3a189262574f2b7
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-27 00:01:30 +0600

    [ADD] id pass function

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -11,13 +11,16 @@ interface PopupProps {
 }
 function Popup({ isOpen, onClose, deleteId, deleteTitle }: PopupProps) {
 	if (!isOpen) return null;
+	const handleDeleted = (id: string) => {
+		console.log(`click${id}`);
+	}
 	return (
 		<div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-500 bg-opacity-50">
 			<div className="p-8 bg-white rounded-lg">
 				<h2 className="mb-4 text-xl font-semibold">{deleteTitle}</h2>
 				<p>This is the content of the popup.</p>
 				<div className="flex justify-around">
-					<button className="px-4 py-2 mt-4 text-white bg-red-500 rounded-lg">
+					<button onClick={() => handleDeleted(deleteId)} className="px-4 py-2 mt-4 text-white bg-red-500 rounded-lg">
 						Yes
 					</button>
 					<button className="px-4 py-2 mt-4 text-white bg-green-500 rounded-lg" onClick={onClose}>
