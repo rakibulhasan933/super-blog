dd6f38a4aec6018cb01efacd8cfaea7dcc47c1fb
rakibulhasan933 <rakibulmd933@gmail.com>
2023-07-27 17:50:51 +0600

    [ADD] updated blog set form

--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,5 +1,5 @@
 import UpdateForm from '@/components/UpdateForm';
-import { ParamsIProps } from '@/type';
+import { BlogProps, ParamsIProps } from '@/type';
 
 async function getSingleBlog(id: string) {
 	const res = await fetch(`http://localhost:3000/api/posts/${id}`);
@@ -9,13 +9,13 @@ async function getSingleBlog(id: string) {
 async function UpdatedBlog({ params }: ParamsIProps) {
 	const { id } = params;
 	const data = await getSingleBlog(id);
-	console.log(data, 'single blogs update page');
+	const blog: BlogProps = data.blog;
 	return (
 		<div className='mx-[40px]'>
 			<div className='flex flex-col px-6'>
 				<h2 className='mb-10 text-2xl font-semibold'>Update Blogs</h2>
 				<div className="flex flex-col gap-2">
-					<UpdateForm />
+					<UpdateForm updateBlog={blog} />
 				</div>
 			</div>
 		</div>
--- src/components/UpdateForm.tsx
+++ src/components/UpdateForm.tsx
@@ -1,9 +1,12 @@
 "use client"
 import React from 'react'
-import { Inputs } from '@/type';
+import { BlogProps, Inputs } from '@/type';
 import { useForm, SubmitHandler } from "react-hook-form";
-
-function UpdateForm() {
+export interface DataIProps {
+	updateBlog: BlogProps
+}
+function UpdateForm({ updateBlog }: DataIProps) {
+	const { title, imageUrl, description } = updateBlog;
 	const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 	const onSubmit: SubmitHandler<Inputs> = async (data) => {
 		try {
@@ -20,17 +23,22 @@ function UpdateForm() {
 		}
 	}
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Title' {...register("title", { required: true })} />
-			{errors.title && <span className='my-2 text-red-600'>This field is required</span>}
-			<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your Image url' {...register("imageUrl", { required: true })} />
-			{errors.imageUrl && <span className='text-red-600'>This field is required</span>}
-			<textarea typeof='string' cols={20} rows={5} className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your message' {...register("description", { required: true })} />
-			<br />
-			{errors.description && <span className='text-red-600'>This field is required</span>}
-			<br />
-			<input className='px-4 py-2 my-3 font-bold border-2 border-blue-500 rounded-md cursor-pointer max-w-fit hover:bg-blue-600 hover:text-white' type="submit" />
-		</form>
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<label className='px-2 my-2 text-base font-semibold'>Current:- <span className="text-blue-400 ">{title}</span> </label>
+				<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Title' {...register("title", { required: true })} />
+				{errors.title && <span className='my-2 text-red-600'>This field is required</span>}
+				<label className='px-2 my-2 text-base font-semibold'>Current:- <span className="text-blue-400 ">{imageUrl}</span> </label>
+				<input className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your Image url' {...register("imageUrl", { required: true })} />
+				{errors.imageUrl && <span className='text-red-600'>This field is required</span>}
+				<label className='px-2 my-2 text-base font-semibold'>Current:- <span className="text-blue-400 ">{description}</span> </label>
+				<textarea typeof='string' cols={20} rows={5} className='w-full px-4 py-2 my-2 text-lg font-medium border-2 border-teal-400 rounded-md bg-slate-50' placeholder='Enter your message' {...register("description", { required: true })} />
+				<br />
+				{errors.description && <span className='text-red-600'>This field is required</span>}
+				<br />
+				<input className='px-4 py-2 my-3 font-bold border-2 border-blue-500 rounded-md cursor-pointer max-w-fit hover:bg-blue-600 hover:text-white' type="submit" />
+			</form>
+		</>
 	)
 }
 
