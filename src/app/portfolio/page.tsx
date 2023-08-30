3c0abdc1875b0d51256a1d108c28fddaddb65eda
rakibulhasan933 <rakibulmd933@gmail.com>
2023-08-31 03:04:07 +0600

    [ADD] some change

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,7 +5,7 @@ function Portfolio() {
 		<div className='my-20'>
 			<div className="px-8 my-4">
 				<h1 className="text-5xl font-bold">Our Works</h1>
-				<p className="px-2 text-base font-bold ">Choose a Gallery</p>
+				<p className="px-2 text-base font-bold "> Choose a Gallery</p>
 			</div>
 		</div>
 	)
