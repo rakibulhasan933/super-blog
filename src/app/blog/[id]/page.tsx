7c1562f84b828dfece8f8c89dbd3657822dfd940
rakibulhasan933 <rakibulmd933@gmail.com>
2023-09-04 22:06:00 +0600

    [ADD] link added

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 
 async function getBlog(id: string) {
-	const res = await fetch(`/api/posts/${id}`, {
+	const res = await fetch(`https://thethroughtfulpen.vercel.app/api/posts/${id}`, {
 		cache: "no-store"
 	});
 	if (!res.ok) {
