70f562d3bbaacd203ca2bb205fb7ab53b3633353
rakibulhasan933 <rakibulmd933@gmail.com>
2023-09-04 14:20:45 +0600

    [ADD] sever

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 // Blogs Data loaded
 async function getData() {
-	const res = await fetch("/api/posts", {
+	const res = await fetch("http://localhost:3000/api/posts", {
 		cache: "no-store",
 	});
 	if (!res.ok) {
